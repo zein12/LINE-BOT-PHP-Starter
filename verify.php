be4e6e7725145155f7f7b501a381e6807fa4d080
zein12 <jena.zein@gmail.com>
2017-02-08 03:40:26 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -6,5 +6,6 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
-$result = curl_exec($ch); curl_close($ch);
+$result = curl_exec($ch); 
+curl_close($ch);
 echo $result;
