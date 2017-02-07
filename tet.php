7cc26dfa2f3f4811e6e5412dba103fb744cea401
zein12 <jena.zein@gmail.com>
2017-02-08 03:28:08 +0700

    Update tet.php

--- tet.php
+++ tet.php
@@ -2,7 +2,7 @@
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<3/cEBpOR0mjAMUtnHKrSrx3N6FnMVNPYfXBIwMO6HNGaljxuxTxZz2fGrmZYFwqfV3dvAWMa7FEGrmOONfbZ7or1wxYgpjbtFMS0Mkk+RftjvYSrUpThxAHGiivf2M662z2zM5P8BSKby0dJiBG3GQdB04t89/1O/w1cDnyilFU=>');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '< a6b4b1a80d9f25eb0a719fc92cef7d86 >']);
-$response = $bot->getMessageContent('<messageId>');
+$response = $bot->getMessageContent('<1497601714>');
 if ($response->isSucceeded()) {
     $tempfile = tmpfile();
     fwrite($tempfile, $response->getRawBody());
